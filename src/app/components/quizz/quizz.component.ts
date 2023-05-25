dc6bfe22cfe07d0fdbe264cf686612eb383093bb
Marcelo Carvalho <marcelo.santana@edu.sc.senai.br>
2023-05-24 21:27:08 -0300

    finalizado

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import quizz_questions from '../../../assets/data/quizz_questions.json';
+
 
 @Component({
   selector: 'app-quizz',
@@ -6,5 +8,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./quizz.component.css']
 })
 export class QuizzComponent {
+  title: string = ""
+  questions: any
+  questionSelected: any
+  answers:string[] = []
+  answerSelected: string = ""
 
+  questionIndex: number = 0
+  questionMaxIndex: number = 0 
+  finished: boolean = false
+
+  constructor() {}
+
+  ngOnInit(): void {
+    if(quizz_questions) {
+      this.finished = false;
+      this.title = quizz_questions.title;
+
+      this.questions = quizz_questions.questions;
+      this.questionSelected = this.questions[this.questionIndex]
+
+      this.questionIndex = 0
+      this.questionMaxIndex = this.questions.length
+    }
+  }
+  playerChoice(alias:string) {
+    this.answers.push(alias)
+    this.nextStep();
+  }
+  async nextStep() {
+    this.questionIndex+=1
+    if(this.questionMaxIndex > this.questionIndex) {
+      this.questionSelected = this.questions[this.questionIndex]
+    } else {
+      const finalAnswer:string = await this.checkResult(this.answers)
+      this.finished = true
+      this.answerSelected = quizz_questions.results[finalAnswer as keyof 
+      typeof quizz_questions.results]
+    }
+  }
+
+  async checkResult(answers:string[]) {
+    const result = answers.reduce((previous, current, i, arr)=>{
+      if(
+        arr.filter(a=>a===previous).length > 
+        arr.filter(a=>a===current).length
+      ) {
+        return previous
+      } else {
+        return current
+      }
+    })
+    return result
+  }
 }
